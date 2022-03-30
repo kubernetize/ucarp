fd62d6f2599d2d1ec2944c1c7b71057f7c97a2a4
Richard Kojedzinszky <richard@kojedz.in>
2022-03-30 09:26:03 +0200

    Guess interface, verbose startup messages

--- assets/usr/local/sbin/ucarp.sh
+++ assets/usr/local/sbin/ucarp.sh
@@ -5,8 +5,24 @@ if [ -z "$UCARP_ADDR" ]; then
 	exit 1
 fi
 
+if [ -z "$UCARP_INTERFACE" ]; then
+	echo "[=] Guessing interface based on default route"
+	UCARP_INTERFACE="$(ip route show | sed -n -r -e 's/^default via.*dev ([^ ]+).*/\1/p')"
+	if [ -z "$UCARP_INTERFACE" ]; then
+		echo "[-] Guessing interface failed"
+		exit 2
+	fi
+	echo "[+] Guessed interface: $UCARP_INTERFACE"
+fi
+
 if [ -z "$UCARP_SRCIP" ]; then
+	echo "[=] Guessing source ip on $UCARP_INTERFACE"
 	UCARP_SRCIP="$(ip addr show dev $UCARP_INTERFACE | awk '/inet / {split($2, a, "/"); print a[1]; exit}')"
+	if [ -z "$UCARP_SRCIP" ]; then
+		echo "[-] Guessing source ip on $UCARP_INTERFACE failed"
+		exit 3
+	fi
+	echo "[+] Guessed source ip on $UCARP_INTERFACE: $UCARP_SRCIP"
 fi
 
 exec /usr/sbin/ucarp \
